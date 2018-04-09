f83f44972cc4c815cc440c7fd46bf1c22945b19f
Lucien LACAZE <llacaze@e2r4p10.42.fr>
2018-04-09 19:03:15 +0200

    a apepeler

--- sources/signals.c
+++ sources/signals.c
@@ -6,14 +6,14 @@
 /*   By: llacaze <llacaze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 17:43:05 by llacaze           #+#    #+#             */
-/*   Updated: 2018/04/09 19:00:35 by llacaze          ###   ########.fr       */
+/*   Updated: 2018/04/09 19:03:07 by llacaze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/sh21.h"
 
 /*
-** A apapeler :
+** To call :
 ** sig_handler with the signal function signal(SIGINT, sig_handler) when child pid is different of -1 after the fork
 ** and before the wait of the adress of the pid;
 ** sig_hand_emp as same as the other function but this time you put it just before the read
